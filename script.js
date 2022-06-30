c1b0035d8517e9f77c4d1de33dfa6a4a55f8ae40
villaricadianne <91996994+villaricadianne@users.noreply.github.com>
2022-06-29 23:22:44 -0700

    Completed hover and creation functions

--- script.js
+++ script.js
@@ -4,23 +4,25 @@ const container = document.querySelector(".container");
 
 // start a new sketch
 function newSketch() {
-  // prompt the user for a number but limit it to 100
-  let userNum = 2;
-  let gridNum = userNum * userNum; // test only
-
   // create a 16x16 grid
-  for (let i = 0; i < 5; i++) {
+  for (let i = 0; i <= 256; i++) {
     const clone = square.cloneNode(true);
     container.appendChild(clone);
-    // clone.style.backgroundColor = "pink";
-    // square.cloneNode(true);
   }
+  hoverSquares();
 
-  // when user clicks on the button
+  // when user clicks on the new button
   document.querySelector(".new").addEventListener("click", createSquares);
 
   function createSquares() {
-    console.log("createSquares");
+    // prompt the user for a number but limit it to 100
+    let userNum = 0;
+    do {
+      userNum = Number(prompt("Please enter a number between 1 to 100"));
+      if (userNum === null) return;
+    } while (typeof userNum === "string" || userNum > 100 || userNum === 0);
+
+    let gridNum = userNum * userNum; // test only
 
     // remove the previous squares
     function clearSquares() {
@@ -31,27 +33,34 @@ function newSketch() {
 
     //calcuate grdiNum W x H
     function calculateSquares() {
-      square.style.width = "100px"; // test only
-      square.style.height = "100px"; // test only
+      square.style.width = "50px"; // test only
+      square.style.height = "50px"; // test only
     }
 
     function cloneSquares() {
-      console.log("cloneSquares");
-      // clone the sqaure based on gridNum
+      // clone the square based on gridNum
       for (let i = 0; i < gridNum; i++) {
         console.log(gridNum);
         const clone = square.cloneNode(true);
         container.appendChild(clone);
-      } // make this much squares, spread evenly across the container
+      }
     }
     clearSquares();
     calculateSquares();
     cloneSquares();
+    hoverSquares();
   }
 
   function hoverSquares() {
-    console.log("hoverSquare");
     // listen for hover, each hover adds 10% darker to a square until its 100% black.
+    const squares = document.querySelectorAll(".square");
+
+    squares.forEach((square) => {
+      square.addEventListener("mouseover", function () {
+        this.style.backgroundColor = "black";
+        this.style.opacity = (parseFloat(this.style.opacity) || 0) + 0.1;
+      });
+    });
   }
 }
 
