762e7e9dfcebc0685cf377611b26b453fe0f61da
villaricadianne <91996994+villaricadianne@users.noreply.github.com>
2022-07-09 15:14:14 -0700

    Finished code

--- script.js
+++ script.js
@@ -5,24 +5,30 @@ const container = document.querySelector(".container");
 // start a new sketch
 function newSketch() {
   // create a 16x16 grid
-  for (let i = 0; i <= 256; i++) {
+  square.style.width = container.offsetWidth / 16 - 2 + "px";
+  square.style.height = container.offsetHeight / 16 - 2 + "px";
+
+  for (let i = 1; i <= 256; i++) {
     const clone = square.cloneNode(true);
     container.appendChild(clone);
   }
+
   hoverSquares();
 
   // when user clicks on the new button
-  document.querySelector(".new").addEventListener("click", createSquares);
+  document.querySelector(".button").addEventListener("click", createSquares);
 
   function createSquares() {
-    // prompt the user for a number but limit it to 100
+    // prompt the user for a number but limit it to 50
     let userNum = 0;
     do {
-      userNum = Number(prompt("Please enter a number between 1 to 100"));
+      userNum = Number(prompt("Please enter a number between 1 to 50"));
       if (userNum === null) return;
-    } while (typeof userNum === "string" || userNum > 100 || userNum === 0);
+    } while (typeof userNum === "string" || userNum > 50 || userNum === 0);
 
-    let gridNum = userNum * userNum; // test only
+    let gridNum = userNum * userNum;
+
+    // document.getElementsByTagName("p").innerHTML(`${userNum}x${userNum}`);
 
     // remove the previous squares
     function clearSquares() {
@@ -31,20 +37,18 @@ function newSketch() {
       }
     }
 
-    //calcuate grdiNum W x H
     function calculateSquares() {
-      square.style.width = "50px"; // test only
-      square.style.height = "50px"; // test only
+      square.style.width = container.offsetWidth / userNum - 2 + "px";
+      square.style.height = container.offsetHeight / userNum - 2 + "px";
     }
-
     function cloneSquares() {
       // clone the square based on gridNum
       for (let i = 0; i < gridNum; i++) {
-        console.log(gridNum);
         const clone = square.cloneNode(true);
         container.appendChild(clone);
       }
     }
+
     clearSquares();
     calculateSquares();
     cloneSquares();
@@ -52,13 +56,21 @@ function newSketch() {
   }
 
   function hoverSquares() {
-    // listen for hover, each hover adds 10% darker to a square until its 100% black.
+    // randomize color option
+    function randomColor() {
+      let o = Math.round,
+        r = Math.random,
+        s = 255;
+      return "rgba(" + o(r() * s) + "," + o(r() * s) + "," + o(r() * s);
+    }
+
+    let color = randomColor();
     const squares = document.querySelectorAll(".square");
 
     squares.forEach((square) => {
       square.addEventListener("mouseover", function () {
-        this.style.backgroundColor = "black";
-        this.style.opacity = (parseFloat(this.style.opacity) || 0) + 0.1;
+        this.style.backgroundColor = color;
+        this.style.opacity = (parseFloat(this.style.opacity) || 0) + 0.2;
       });
     });
   }
